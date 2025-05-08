e2e345259556fbb9234161d6218c88db03ada5a9
Maxim <yrev2011@gmail.com>
2025-05-08 12:49:02 +0700

    Edit generator annotation

--- src/core/converter.py
+++ src/core/converter.py
@@ -7,6 +7,6 @@ from src.core.model import Bulletin
 type DataFrame = NDFrame
 
 
-def covert_df_to_model_gen(df: DataFrame) -> Generator[Bulletin, Any, None]:
+def covert_df_to_model_gen(df: DataFrame) -> Generator[Bulletin, None, None]:
     for _, row in df.iterrows():
         yield Bulletin.from_df_row(row)
